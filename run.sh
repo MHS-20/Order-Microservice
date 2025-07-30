931a3826613f30fb1865cd644c52cdc5574619dd
MHS <muhamad.salman2001@gmail.com>
2025-07-30 20:14:29 +0200

    chore: fixed type

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ export DATA_SOURCE_URL="root:verysecretpass@tcp(127.0.0.1:3306)/order"
 export APPLICATION_PORT=3000 
 export ENV=development 
 go run cmd/main.go
+
+grpcurl -d '{"user_id": 123, "order_items": [{"product_code": "prod", "quantity": 4, "unit_price": 12}]}' -plaintext localhost:3000 Order/Create
