89eabd885260e1c6551f530f81a24d1175f1edb0
MHS <muhamad.salman2001@gmail.com>
2025-07-30 19:02:06 +0200

    feat: added main and run script

---
+++ order/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/MHS-20/Order-Microservice/order/config"
+	"github.com/MHS-20/Order-Microservice/order/internal/adapters/db"
+	"github.com/MHS-20/Order-Microservice/order/internal/adapters/grpc"
+	"github.com/MHS-20/Order-Microservice/order/internal/application/core/api"
+)
+
+func main() {
+	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
+	if err != nil {
+		log.Fatalf("Failed to connect to database. Error: %v", err)
+	}
+	application := api.NewApplication(dbAdapter)
+	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+	grpcAdapter.Run()
+}
--- run.sh
+++ run.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
+docker run -p 3306:3306 \
+ -e MYSQL_ROOT_PASSWORD=verysecretpass \
+ -e MYSQL_DATABASE=order mysql
 
-SERVICE_NAME=$1
-RELEASE_VERSION=$2
-
-sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-
-protoc --go_out=./golang --go_opt=paths=source_relative \
- --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
- ./${SERVICE_NAME}/*.proto
-
-cd golang/${SERVICE_NAME}
-go mod init github.com/MHS-20/order-proto/golang/${SERVICE_NAME} ||true
-go mod tidy
-
-cd ../../
-git config --global user.email "muhamad.salman2001@gmail.com"
-git config --global user.name "MHS-20"
-
-git add . && git commit -am "proto update" || true
-git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
- -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
-git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+export DATA_SOURCE_URL="root:verysecretpass@tcp(127.0.0.1:3306)/order"
+export APPLICATION_PORT=3000 
+export ENV=development 
+go run cmd/main.go
