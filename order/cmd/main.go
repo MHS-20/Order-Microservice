9e05c0e18abb1e25a2b89c192ba8bfabb21414c8
MHS <muhamad.salman2001@gmail.com>
2025-07-31 12:21:04 +0200

    feat: added payment call and initialization

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/MHS-20/Order-Microservice/order/config"
 	"github.com/MHS-20/Order-Microservice/order/internal/adapters/db"
 	"github.com/MHS-20/Order-Microservice/order/internal/adapters/grpc"
+	"github.com/MHS-20/Order-Microservice/order/internal/adapters/payment"
 	"github.com/MHS-20/Order-Microservice/order/internal/application/core/api"
 )
 
@@ -14,7 +15,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to connect to database. Error: %v", err)
 	}
-	application := api.NewApplication(dbAdapter)
+
+	paymentAdapter, err := payment.NewAdapter(config.GetPaymentServiceUrl())
+	if err != nil {
+		log.Fatalf("Failed to initialize payment stub. Error: %v", err)
+	}
+
+	application := api.NewApplication(dbAdapter, paymentAdapter)
 	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
 	grpcAdapter.Run()
 }
