6600dc1a8c3115a1aa4fa321244248d62c9de376
MHS <muhamad.salman2001@gmail.com>
2025-07-31 15:12:02 +0200

    feat: added error handling in order

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -1,8 +1,13 @@
 package api
 
 import (
+	"context"
+
 	"github.com/MHS-20/Order-Microservice/order/internal/application/core/domain"
 	"github.com/MHS-20/Order-Microservice/order/internal/ports"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Application struct {
@@ -17,15 +22,23 @@ func NewApplication(db ports.DBPort, payment ports.PaymentPort) *Application {
 	}
 }
 
-func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
+func (a Application) PlaceOrder(ctx context.Context, order domain.Order) (domain.Order, error) {
 	err := a.db.Save(&order)
 	if err != nil {
 		return domain.Order{}, err
 	}
-
 	paymentErr := a.payment.Charge(&order)
 	if paymentErr != nil {
-		return domain.Order{}, paymentErr
+		st, _ := status.FromError(paymentErr)
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: st.Message(),
+		}
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+		statusWithDetails, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDetails.Err()
 	}
 	return order, nil
 }
