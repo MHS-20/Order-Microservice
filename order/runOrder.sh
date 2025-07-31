c3b99a556511704d497ce32cdd93d7e754b335ec
MHS <muhamad.salman2001@gmail.com>
2025-07-31 15:21:48 +0200

    feat: updated run scripts

--- order/runOrder.sh
+++ order/runOrder.sh
@@ -5,7 +5,8 @@ docker run -p 3306:3306 \
 
 export DATA_SOURCE_URL="root:verysecretpass@tcp(127.0.0.1:3306)/order"
 export APPLICATION_PORT=3000 
-export ENV=development 
+export ENV=development
+export PAYMENT_SERVICE_URL=localhost:3001
 go run cmd/main.go
 
 grpcurl -d '{"user_id": 123, "order_items": [{"product_code": "prod", "quantity": 4, "unit_price": 12}]}' -plaintext localhost:3000 Order/Create
